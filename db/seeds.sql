34d6c5ec626c7d18869b1536fa289388736f6119
reinarivera16 <72185375+reinarivera16@users.noreply.github.com>
2021-01-23 23:40:29 -0600

    Add files via upload
    adding db

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bean');
+INSERT INTO burgers (name) VALUES ('Veggie');
+INSERT INTO burgers (name, devoured) VALUES ('Meaty', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bison', true);
+INSERT INTO burgers (name, devoured) VALUES ('Cheesy', true);
+INSERT INTO burgers (name) VALUES ('Lamb');
